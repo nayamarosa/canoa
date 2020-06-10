cfcb99d4402e363da4af3d1afd45756b63e7a50f
NayamaRosa <falecom@nayama.com.br>
2020-06-10 13:34:00 -0300

    estilo dos botões de borda, botões com preenchimento e botões ícones

--- src/components/base/Button/Button.jsx
+++ src/components/base/Button/Button.jsx
@@ -3,5 +3,8 @@ import React from 'react';
 import './Button.scss';
 
 export default function Button(props) {
-    return <button type={props.type} className={`btn ${props.className}`}>{props.text}</button>
+    return <button type={props.type} className={`btn ${props.className}`}>
+        {props.text}
+        <i className={props.icon}></i>
+        </button>
 }
--- src/components/base/Input/Input.jsx
+++ src/components/base/Input/Input.jsx
@@ -3,5 +3,5 @@ import React from 'react';
 import './Input.scss';
 
 export default function Input(props) {
-    return <input type={props.type} className={`input ${props.className}`}></input>
+    return <input type={props.type} placeholder={props.placeholder} className={`input ${props.className}`}></input>
 }
