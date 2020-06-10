6bbc0202868c68da04979e8f8d87f131aa85dbb7
NayamaRosa <falecom@nayama.com.br>
2020-06-10 15:36:21 -0300

    estilo de card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,19 +6,30 @@ import Button from '../base/Button'
 
 export default function Card() {
   return (
-    <a href="/">
-      <li>
-        <img src="" alt=""/>
-        <span>50% off</span>
-        <h4>Nome da roupa</h4>
-        <span>preço antigo</span>
-        <p>preço novo</p>
-        <Button 
-          type="submit"
-          className="btn__primary btn__primary-buy"
-          text="Comprar"
-        />
-      </li>
-    </a>
+    <li className="card__item">
+      <a href="/">
+        <div className="card__item-image-group">
+          <img src="https://viniciusvinna.netlify.app/assets/api-fashionista/20002605_615_catalog_1.jpg" alt=""/>
+          <div className="card__item-image-group--discount">
+          <span>50%</span>
+          <span>OFF</span>
+          </div>
+        </div>
+        <div className="card__item-text-group">
+          <h4>Nome da roupa</h4>
+          <div className="card__item-text-group-align">
+            <div className="card__item-text-group-price">
+              <span>R$ 200</span>
+              <p>R$ 100</p>
+            </div>
+            <Button 
+              type="submit"
+              className="btn__primary btn__primary-buy"
+              text="Comprar"
+            />
+          </div>
+        </div>
+      </a>
+    </li>
   )
 }
