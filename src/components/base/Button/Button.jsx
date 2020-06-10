bc60cf02e4c0c08395e8d7a502c045faa2d1885e
NayamaRosa <falecom@nayama.com.br>
2020-06-10 13:55:35 -0300

    input e estilo de input

--- src/components/base/Button/Button.jsx
+++ src/components/base/Button/Button.jsx
@@ -3,8 +3,50 @@ import React from 'react';
 import './Button.scss';
 
 export default function Button(props) {
-    return <button type={props.type} className={`btn ${props.className}`}>
-        {props.text}
-        <i className={props.icon}></i>
-        </button>
+  return <button type={props.type} className={`btn ${props.className}`}>
+            {props.text}
+            <i className={props.icon}></i>
+          </button>
 }
+
+// Amostras dos botões
+// Botões com borda
+//   <Button 
+//     type="submit"
+//     className="btn__primary btn__primary-buy"
+//     text="Comprar"
+//   />
+//   <Button 
+//     className="btn__primary btn__primary-size"
+//     text="PP"
+//   />
+
+// Botões com preenchimento
+//   <Button 
+//     type="submit"
+//     className="btn__secondary btn__secondary--blue"
+//     text="Adicionar ao carrinho"
+//     icon="fas fa-shopping-cart"
+//   />
+//   <Button 
+//     type="submit"
+//     className="btn__secondary btn__secondary--green"
+//     text="Finalizar compra"
+//   />
+
+// Botões ícones
+//   <Button 
+//     type="submit"
+//     className="btn__icon"
+//     icon="fas fa-search"
+//   />
+//   <Button 
+//     type="submit"
+//     className="btn__icon btn__icon--pink"
+//     icon="fas fa-times"
+//   />
+//   <Button 
+//     type="submit"
+//     className="btn__icon"
+//     icon="fas fa-shopping-cart"
+//   />
--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -3,56 +3,13 @@ import React from 'react';
 import './App.scss';
 
 // import Input from '../../components/base/Input';
-import Button from '../../components/base/Button';
+// import Button from '../../components/base/Button';
 
 
 function App() {
   return (
     <>
-      {/* <Input 
-      type="text"
-      placeholder="o que você procura?"
-      className="input__search"
-      /> */}
-      <div>
-        <Button 
-          type="submit"
-          className="btn__primary btn__primary-buy"
-          text="Comprar"
-        />
-        <Button 
-          className="btn__primary btn__primary-size"
-          text="PP"
-        />
-      </div>
-      <div>
-      <Button 
-          type="submit"
-          className="btn__secondary btn__secondary--blue"
-          text="Adicionar ao carrinho"
-          icon="fas fa-shopping-cart"
-        />
-        <Button 
-          type="submit"
-          className="btn__secondary btn__secondary--green"
-          text="Finalizar compra"
-        />
-      </div>
-      <Button 
-        type="submit"
-        className="btn__icon"
-        icon="fas fa-search"
-      />
-      <Button 
-        type="submit"
-        className="btn__icon btn__icon--pink"
-        icon="fas fa-times"
-      />
-      <Button 
-        type="submit"
-        className="btn__icon"
-        icon="fas fa-shopping-cart"
-      />
+      
     </>
   );
 }
