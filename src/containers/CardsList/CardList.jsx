922b2d2e7c9918c43dce68ba83b109172d3c7dea
NayamaRosa <falecom@nayama.com.br>
2020-06-10 18:35:25 -0300

    componente base hero-banner, ajuste de container nos componentes

--- src/containers/CardsList/CardList.jsx
+++ src/containers/CardsList/CardList.jsx
@@ -3,5 +3,5 @@ import React from 'react';
 import '../../components/Card/Card.scss'
 
 export default function CardList(props) {
-return <ul className="card__list">{props.children}</ul>
+return <ul className="container card__list">{props.children}</ul>
 }
