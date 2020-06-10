d982f9a407e8ba99de71405b7a371f6b32397d62
NayamaRosa <falecom@nayama.com.br>
2020-06-10 18:20:46 -0300

    cards adjusts

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -3,12 +3,21 @@ import React from 'react';
 import './App.scss';
 
 import Header from '../../components/Header';
+import Card from '../../components/Card';
+
+import CardList from '../CardsList';
 
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <CardList>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </CardList>
     </div>
   );
 }
