4fae8d528ece89dfb789d9b873780ae02dbd829a
NayamaRosa <falecom@nayama.com.br>
2020-06-11 14:47:31 -0300

    ajuste do product info para o card e para a página do produto

--- src/components/base/ProductInfo/ProductInfo.jsx
+++ src/components/base/ProductInfo/ProductInfo.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 
 import './ProductInfo.scss';
 
+import Button from '../Button';
+
 export default function ProductInfo(props) {
   return (
-    <div className="product-info__price">
-      <p>
-        {props.oldPriceText} <span className="product-info__price--old">{props.oldPriceValue}</span>
-      </p>
-      <p>
-        Por <span className="product-info__price--new">{props.priceValue}</span>
-      </p>
-      <p>
-        {props.installmentsText} <span>{props.installments}</span>
-      </p>
+    <>
+    <div className={`product-info__price-group ${props.classNamePriceGroup}`}>
+      <h4>{props.productName}</h4>
+      <div className="product-info__price-group-align">
+        <div className={`product-info__price ${props.classNamePrice}`}>
+          <p>
+            {props.oldPriceText} <span className={`product-info__price--old ${props.classNameOldPrice}`}>{props.oldPriceValue}</span>
+          </p>
+          <div className="product-info__price-group-align">
+            <p>
+              Por <span className={`product-info__price--new ${props.classNameNewPrice}`}>{props.priceValue}</span>
+            </p>
+            <p className="product-info__price--bigger--installments">
+              {props.installmentsText} <span>{props.installments}</span>
+            </p>
+          </div>
+        </div>
+        <Button 
+          type={props.type}
+          classNameBtn={props.classNameBtn}
+          text={props.text}
+        />
+      </div>
     </div>
+    </>
     )
   }
--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -3,32 +3,44 @@ import React from 'react';
 import './App.scss';
 
 import Banner from '../../components/base/Banner';
+import ProductInfo from '../../components/base/ProductInfo';
 
 import Header from '../../components/Header';
 import NavFilter from '../../components/NavFilter';
-import Card from '../../components/Card';
+import Card from '../../components/Card'
 
-import CardList from '../CardsList';
-
+import Product from '../../containers/Product'
 
 function App() {
   return (
     <>
-      <Header />
-      <Banner 
-        className="banner__hero"
+    <Header />
+    <NavFilter />
+    <Card />
+    <Product>
+      <Banner
+        className="container banner__product"
         src="https://viniciusvinna.netlify.app/assets/api-fashionista/20002605_615_catalog_1.jpg"
-        alt="Hero Banner"
+        alt="Nome da roupa"
+        classNameDiscount="banner__product--discount banner__product--discount-bigger"
+        percent= "50%"
       />
-      <NavFilter />
-      <CardList>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </CardList>
+      <ProductInfo
+        classNamePriceGroup="container product-info__price-group--bigger"
+        classNamePrice="product-info__price--bigger"
+        classNameOldPrice="product-info__price--bigger--old"
+        classNameNewPrice="product-info__price--bigger--new"
+        productName="Nome da Roupa"
+        oldPriceText="De"
+        oldPriceValue="R$ 200"
+        priceValue="R$ 100"
+        installmentsText="Em até"
+        installments="3x de R$ 33,33"
+      />
+    </Product>
     </>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
+  
