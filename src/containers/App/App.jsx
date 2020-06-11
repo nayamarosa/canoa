2d64c790881ab53c5bea29e46e0762aaaa1658fb
NayamaRosa <falecom@nayama.com.br>
2020-06-11 11:32:52 -0300

    route home html

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -2,30 +2,15 @@ import React from 'react';
 
 import './App.scss';
 
-import HeroBanner from '../../components/base/HeroBanner';
-
 import Header from '../../components/Header';
-import NavFilter from '../../components/NavFilter';
-import Card from '../../components/Card';
-
-import CardList from '../CardsList';
 
 
 function App() {
   return (
     <>
       <Header />
-      <HeroBanner 
-        src="https://viniciusvinna.netlify.app/assets/api-fashionista/20002605_615_catalog_1.jpg"
-        alt="Hero Banner"
-      />
-      <NavFilter />
-      <CardList>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </CardList>
+      
+
     </>
   );
 }
