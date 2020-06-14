daadfdd15da433c38cf07ff1a0838cecdde7c93e
NayamaRosa <falecom@nayama.com.br>
2020-06-14 15:03:45 -0300

    aplicando estilo nos filtros e mostrando texto do yipo de filtro

--- src/components/NavFilter/NavFilter.jsx
+++ src/components/NavFilter/NavFilter.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './NavFilter.scss';
 
-export default function NavFilter(props) {
+const NavFilter = (props) => {
+  const [active, setActive] = useState(null);
+
+  const handleActiveFilter = (e) => {
+    e.preventDefault();
+    let activeFilter = e.target;
+    let activeFilterText = activeFilter.textContent;
+    let filterText = document.querySelector('.nav-filter__name');
+    filterText.innerHTML = activeFilterText
+
+    activeFilter.classList.add("nav-filter__item--active");
+    setActive(e.target);
+
+    if (active !== e.target) {
+      setActive(e.target);
+    }
+    if (active) {
+      active.classList.remove("nav-filter__item--active");
+    }
+  }
+
   return (
     <>
     <nav className="nav-filter">
       <ul className="nav-filter__list">
         <li className="nav-filter__item">
-          <a href="/">
-            <h2 className="nav-filter__item--active">Coleção nova</h2>
+          <a href="/" onClick={(e) => handleActiveFilter(e)}>
+            <h2>Coleção nova</h2>
           </a>
         </li>
         <li className="nav-filter__item">
-          <a href="/">
+          <a href="/" onClick={(e) => handleActiveFilter(e)}>
             <h2>Promoções</h2>
           </a>
         </li>
       </ul>
     </nav>
-    <h3 className="container nav-filter__name">{props.selectedFilterName}</h3>
+    <h3 className="container nav-filter__name">&ensp;</h3>
     </>
   )
 }
+
+export default NavFilter;
--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.scss';
 
 import Header from '../../components/Header';
+import NavFilter from '../../components/NavFilter';
+
 
 function App() {
   return (
     <>
     <Header />
-    
+    <NavFilter />
     </>
     );
   }
