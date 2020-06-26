80841dc90878f87ea851d534e1c93de378ca1a58
sirleyalmeida <sirleyas90@gmail.com>
2020-06-25 23:36:39 -0300

    rota do carrinho iniciada

--- src/components/ProductSize/ProductSize.jsx
+++ src/components/ProductSize/ProductSize.jsx
@@ -11,9 +11,6 @@ export default function ProductSize(props) {
       <form className="product-size__group">
         {props.productSizes.map(sizeInfo => <Input info={sizeInfo} key={sizeInfo.sku}
           classNameInputGroup="input__size-position"
-          // {sizeInfo.available === false 
-          // ? classNameLabel="input__size-label"
-          // : classNameLabel="input__size-label"}
           classNameLabel={sizeInfo.available === false ? "input__size-label-notAvailable" : "input__size-label"}          
           label={sizeInfo.size}
           classNameInput={props.classNameInput}
