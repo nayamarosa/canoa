bb698c0059cd42b41f4228f6e26aab8790e58a8c
NayamaRosa <falecom@nayama.com.br>
2020-06-27 16:08:34 -0300

    aplicando estilo na lista

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './CartItem.scss';
 
-// import Button from '../base/Button';
+import Button from '../base/Button';
 import Banner from '../base/Banner';
 
 import ProductInfo from '../ProductInfo';
@@ -9,22 +9,39 @@ import ProductInfo from '../ProductInfo';
 const CartItem = ({product, onClick}) => {
   return (
   <>  
-    <li className="search__item">
-      <a href="/">
+    <li>
+      <a href="/" className="cart__item">
         <Banner 
-          classNameBanner="banner__product banner__product--search"
+          classNameBanner="banner__product--cart"
           src={product.image}
           alt={product.name}
           classNameDiscount="banner__product--search-discount"
           percent={product.discount_percentage}
         />
-        <ProductInfo 
-          classNamePriceGroup="product-info__price-group--search"
-          productName={product.name}
-          oldPriceText="De"
-          oldPriceValue={product.regular_price}
-          priceValue={product.actual_price}
-        />
+        <div>
+          <ProductInfo 
+            classNamePriceGroup="product-info__price-group--search"
+            productName={product.name}
+            oldPriceText="De"
+            oldPriceValue={product.regular_price}
+            priceValue={product.actual_price}
+          />
+          <p>Tamanho: <span>G</span></p>
+          <div>
+            <p>Quantidade:</p>
+            <Button 
+            type="button"
+            classNameBtn="btn__icon"
+            icon="fas fa-minus"
+            />
+            <span>1</span>
+            <Button 
+            type="button"
+            classNameBtn="btn__icon"
+            icon="fas fa-plus"
+            />
+          </div>
+        </div>
       </a>
     </li>
   </>
--- src/containers/CartList/CartList.jsx
+++ src/containers/CartList/CartList.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import CartItem from '../../components/CartItem';
 
 const CartList = ({products}) => {
-  console.log(products)
   return (
     <ul className="container cart__list">
       {products.map(product => <CartItem product={product} key={product.code_color}/>)} 
