4758ef4e74a0f318ce2a7c39abfcbd353aa0fd3f
NayamaRosa <falecom@nayama.com.br>
2020-06-28 12:13:10 -0300

    frase de carrinho vazio

--- src/containers/CartList/CartList.jsx
+++ src/containers/CartList/CartList.jsx
@@ -7,7 +7,11 @@ const CartList = ({products}) => {
   return (
     <ul className="container cart__list">
       <p className="cart__list-title">Seu carrinho</p>
-      {products.map(product => <CartItem product={product} key={product.code_color}/>)} 
+      {
+        products.length > 0
+        ? products.map(product => <CartItem product={product} key={product.code_color}/>)
+        : <p className="cart__list-empty">Seu carrinho ainda está vazio</p>
+        }
     </ul>
   )
 }
