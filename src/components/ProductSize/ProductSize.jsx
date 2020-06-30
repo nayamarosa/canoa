db9427860634aafb6914bb65b721605cbf9c0b11
NayamaRosa <falecom@nayama.com.br>
2020-06-30 13:48:39 -0300

    mensagem de selecionar um tamanho

--- src/components/ProductSize/ProductSize.jsx
+++ src/components/ProductSize/ProductSize.jsx
@@ -7,29 +7,30 @@ import Input from '../base/Input';
 export default function ProductSize(props) {
   return (
     <div className="product-size">
-    <h5>Escolha o tamanho</h5>
-    <form className="product-size__group">
-    {
-      props.productSizes !== undefined 
-      ? props.productSizes.map(sizeInfo => <Input 
-        info={sizeInfo} 
-        key={sizeInfo.sku}
-        classNameInputGroup="input__size-position"
-        classNameLabel={sizeInfo.available === false 
-          ? "input__size-label input__size-label--not-available" 
-          : "input__size-label"}          
-        label={sizeInfo.size}
-        classNameInput={props.classNameInput}
-        type={props.type}
-        id={sizeInfo.size}
-        name="size"
-        value={sizeInfo.size}
-        disabled={sizeInfo.available === false ? true : false}
-        onClick={props.onClick}
-        />)
-        : false
-      }
-      </form>
+      <h5>Escolha o tamanho</h5>
+      <p className="product-size-needed">* Por favor selecione um tamanho</p>
+      <form className="product-size__group">
+      {
+        props.productSizes !== undefined 
+        ? props.productSizes.map(sizeInfo => <Input 
+          info={sizeInfo} 
+          key={sizeInfo.sku}
+          classNameInputGroup="input__size-position"
+          classNameLabel={sizeInfo.available === false 
+            ? "input__size-label input__size-label--not-available" 
+            : "input__size-label"}          
+          label={sizeInfo.size}
+          classNameInput={props.classNameInput}
+          type={props.type}
+          id={sizeInfo.size}
+          name="size"
+          value={sizeInfo.size}
+          disabled={sizeInfo.available === false ? true : false}
+          onClick={props.onClick}
+          />)
+          : false
+        }
+        </form>
       </div>
       )
     }
