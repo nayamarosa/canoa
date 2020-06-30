ed16548db058d803151ff240f525f7ee9ac87ec9
NayamaRosa <falecom@nayama.com.br>
2020-06-30 16:54:41 -0300

    página de checkout

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector, connect } from 'react-redux';
 
 import { fetchCatalog } from '../../actions/catalog';
