081a940de39bdc94ce8989d4695e0483aeb7aa5a
NayamaRosa <falecom@nayama.com.br>
2020-07-01 18:04:39 -0300

    mock json

--- src/__tests__/header.test.js
+++ src/__tests__/header.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history'
 import { render, fireEvent } from '@testing-library/react';
 import { store } from '../store';
-import { Provider, useSelector } from 'react-redux';
-import Header from '../components/Header';
-import SearchList from '../containers/SearchList';
+import { Provider } from 'react-redux';
+
+import Header from '../containers/Header';
+
+import products from './mocks/products.json'
 
 const HeaderTest = () => {
   return (
@@ -13,52 +16,55 @@ const HeaderTest = () => {
         <Header />
       </BrowserRouter>
     </Provider>
-    )
+  )
 }
 
-//testes header
-test('render component header', () => {
-  const { getByTestId } = render(HeaderTest());
-  expect(getByTestId('header')).toBeInTheDocument()
-})
-
-test('nome da marca', () => {
-  const { queryByText } = render(HeaderTest());
-  expect(queryByText('CANOÁ')).toBeInTheDocument()
-})
-
-// test('redireciona para o carrinho', () => {
-//   const { queryByText } = render(HeaderTest());
-//   const cart = queryByText('Carrinho');
-//   fireEvent.click(cart)
-//   expect(queryByPlaceholderText('o que você procura?')).toBeInTheDocument()
-// })
-
-//testes busca
-test('render component search', () => {
-  const { getByTestId , debug} = render(HeaderTest());
-  debug()
-  expect(getByTestId('search-list')).toBeInTheDocument()
-})
-
-test('busca aberta', () => {
-  const { queryByText, queryByPlaceholderText } = render(HeaderTest());
-  const open = queryByText('Abrir busca');
-  fireEvent.click(open)
-  expect(queryByPlaceholderText('o que você procura?')).toBeInTheDocument()
-})
-
-// test('busca fechada', () => {
-//   const { queryByText, queryByPlaceholderText } = render(HeaderTest());
-//   const close = queryByText('Fechar busca');
-//   fireEvent.click(close)
-//   expect(queryByPlaceholderText('o que você procura?')).not.toBeInTheDocument()
-// })
-
-// test('faz busca', () => {
-//   const { queryByText , queryByPlaceholderText } = render(HeaderTest());
-//   const placeholder = queryByPlaceholderText('o que você procura?');
-//   fireEvent.change(placeholder, { target: { value: 'vestido' } })
-//   //faz a funcão
-//   expect(queryByText(placeholder.value)).toBe('VESTIDO TRANSPASSE BOW')
-// })
+const handleClickToCart = (e, code) => {
+  history.push('/carrinho-de-compras');
+}
+  
+  //testes header
+  test('render component header', () => {
+    const { getByTestId } = render(HeaderTest());
+    expect(getByTestId('header')).toBeInTheDocument()
+  })
+  
+  test('nome da marca', () => {
+    const { queryByText } = render(HeaderTest());
+    expect(queryByText('CANOÁ')).toBeInTheDocument()
+  })
+  
+  // test('redireciona para o carrinho', () => {
+  //   const { queryByText } = render(HeaderTest());
+  //   const cart = queryByText('Carrinho');
+  //   fireEvent.click(cart)
+  //   expect(handleClickToCart().toHaveBeenCalledWith('/carrinho-de-compras'))
+  // })
+  
+  //testes busca
+  test('render component search', () => {
+    const { getByTestId } = render(HeaderTest());
+    expect(getByTestId('search-list')).toBeInTheDocument()
+  })
+  
+  test('busca aberta', () => {
+    const { queryByText, queryByPlaceholderText } = render(HeaderTest());
+    const open = queryByText('Abrir busca');
+    fireEvent.click(open)
+    expect(queryByPlaceholderText('o que você procura?')).toBeInTheDocument()
+  })
+  
+  // test('busca fechada', () => {
+  //   const { queryByText, queryByPlaceholderText } = render(HeaderTest());
+  //   const close = queryByText('Fechar busca');
+  //   fireEvent.click(close)
+  //   expect(queryByPlaceholderText('o que você procura?')).not.toBeInTheDocument()
+  // })
+  
+  // test('faz busca', () => {
+  //   const { queryByText , queryByPlaceholderText } = render(HeaderTest());
+  //   const placeholder = queryByPlaceholderText('o que você procura?');
+  //   fireEvent.change(placeholder, { target: { value: 'vestido' } })
+  //   //faz a funcão
+  //   expect(queryByText(placeholder.value)).toBe('VESTIDO TRANSPASSE BOW')
+  // })
